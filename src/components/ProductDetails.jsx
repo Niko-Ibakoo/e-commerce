0e8196c4eae981298aae1373aa18bda1f844874a
Niko <germano.niko01@gmail.com>
2023-03-28 11:55:47 -0400

    fixed Product.js api call with axios

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,52 +1,41 @@
 import "../components/ProductDetails.css";
-import { useState, useEffect, } from 'react';
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-
-//we should take a look at this function
+import axios from "axios";
+//in react we can use a hook called useParams, so we don't need to pass anything in this function as a param. (I'll explain better on Monday)
 function ProductDetails() {
-    const [productData, setProductData] = useState(null);
-    const [error, setError] = useState(null);
-    const params = useParams()
+  const [productData, setProductData] = useState([]);
+  const params = useParams();
 
-    useEffect(() => {
-        fetch(`https://fakestoreapi.com/products/${params.id}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Someone done fucked up');
-                }
-                return response.json();
-        })
-        .then(data => setProductData(data))
-        .catch(error => setError(error));
-    }, []);
+  useEffect(() => {
+    axios
+      .get(`https://fakestoreapi.com/products/${params.id}`)
+      .then((response, err) => {
+        err ? console.log(err) : setProductData(response.data);
+      });
+  }, []);
 
+  // we should create a loading component and import it for conditional rendering everytime we need it
+  // e.g productData ? // do something.. : show <Loading/> I'll show you more about ternary operators and && in React next time
+  // but this is good!
 
+    return (
+      <>
+        {productData? <div className="product-details">
+          <img
+            className="product-image"
+            src={productData.image}
+            alt="This is a product."
+          />
+          <div className="product-information">
+            <h2 className="product-title">{productData.title}</h2>
+            <p className="product-description">{productData.description}</p>
+            <button className="add-to-cart">Add to Cart</button>
+          </div>
+        </div> : <div>loading</div>}
+      </>
+    );
+  }
 
-    // we should create a loading component and import it for conditional rendering everytime we need it 
-    // e.g productData ? // do something.. : show <Loading/> I'll show you more about ternary operators and && in React next time
-    // but this is good! 
-
-    if (error) {
-        return <div>{error}</div>;
-    } else if (!productData) {
-        return <div>Loading...</div>
-    } else {
-        console.log(productData);
-        return (
-            <div className="product-details">
-                <img
-                    className="product-image"
-                    src={productData.image}
-                    alt="This is a product."
-                />
-                <div className="product-information">
-                    <h2 className="product-title">{productData.title}</h2>
-                    <p className="product-description">{productData.description}</p>
-                    <button className="add-to-cart">Add to Cart</button>
-                </div>
-            </div>
-        );
-    }
-}
 
 export default ProductDetails;
