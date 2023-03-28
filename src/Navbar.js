36aa9c93aa26901fca06bf688fea7efe85c29721
Niko <germano.niko01@gmail.com>
2023-03-28 18:30:02 -0400

    improved navbar for desktop view

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/navbar.css";
 import { BsCart2 } from "react-icons/bs";
 import { HiUserCircle } from "react-icons/hi2";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaAmazonPay } from "react-icons/fa";
-import {AiFillCloseCircle} from 'react-icons/ai'
+import { AiFillCloseCircle } from "react-icons/ai";
 const Navbar = () => {
-  const [isActive, setIsActive] = useState(false); //
-  const dynamicWidth = isActive ? `93%` : `5%`; // dynamically changing the width of the search bar
+  const [isActive, setIsActive] = useState(false); // toggle search bar icon
+  const smallWidth = window.innerWidth > 700 ? `2%` : `5%`;
+  const dynamicWidth = isActive ? `93%` : smallWidth; // dynamically changing the width of the search bar
+
   // create nav with logo search bar and cart
   // cart is displayed dynamically based on length on items if >0 then display else : null
   // search bar should slide from the right side on mobile view
@@ -17,12 +19,22 @@ const Navbar = () => {
   return (
     <header>
       <nav>
-        <div className="user"><HiUserCircle/></div>
-        <div className="cart"><BsCart2/></div>
+        <div className="user">
+          <HiUserCircle />
+        </div>
+        <div className="cart">
+          <BsCart2 />
+        </div>
         <div className="search-bar">
           <input style={{ width: dynamicWidth }} type="text" />{" "}
           <div onClick={() => setIsActive(!isActive)}>
-            {isActive? <AiFillCloseCircle className="search"/>: <AiOutlineSearch className="search" />}
+            <div className="search-container">
+              {isActive ? (
+                <AiFillCloseCircle className="search" />
+              ) : (
+                <AiOutlineSearch className="search" />
+              )}
+            </div>
           </div>
         </div>
       </nav>
